448ab5ec1f8cfa13f13b4e5b250c688c2468ef8d
jiancheng <1729303158@qq.com>
2023-05-09 14:24:21 +0800

    feature: 完成图片的显示

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import ListMeta from "@/views/ListMeta.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/list_meta",
+    name: "ListMeta",
+    component: ListMeta,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/ListMeta.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="fetchData">Fetch Data</button>
+    <div v-for="item in items" :key="item.id">
+      <img :src="item.file_url" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get("http://127.0.0.1:6200/meta/image_file")
+        .then((response) => {
+          this.items = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
